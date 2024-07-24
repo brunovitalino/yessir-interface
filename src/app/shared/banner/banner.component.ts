6621f177b363828d15e368ac3fbb2c3f560272f1
Bruno Vitalino <brunovitalino@hotmail.com>
2024-07-24 14:43:28 -0300

    Created banner

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
-
+  @Input() src: String = '' // 'assets/imagens/banner-homepage.png'
+  @Input() alt: String = '' // 'Banner da Aplicação YesSir'
+    
 }
