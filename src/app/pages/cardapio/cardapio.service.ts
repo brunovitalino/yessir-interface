9ac04422da9b305405924843da745c298b64e233
Bruno Vitalino <brunovitalino@hotmail.com>
2024-09-12 14:41:44 -0300

    Created environment profiles

--- src/app/pages/cardapio/cardapio.service.ts
+++ src/app/pages/cardapio/cardapio.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Cardapio } from './cardapio';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,20 @@ import { Observable } from 'rxjs';
 export class CardapioService {
 
   // private readonly API = 'http://localhost:8080/cardapio'
-  private readonly API = 'http://localhost:3000/cardapio'
+  private readonly API_HOST = environment.apiHost;
 
   constructor(private http: HttpClient) { }
 
   listar(): Observable<Cardapio[]> {
-    return this.http.get<Cardapio[]>(this.API);
+    return this.http.get<Cardapio[]>(`${this.API_HOST}/cardapio`);
   }
 
   listarPorNome(varlorDigitado: string): Observable<Cardapio[]> {
     const params = new HttpParams().append('nome', varlorDigitado);
-    return this.http.get<Cardapio[]>(this.API, { params });
+    return this.http.get<Cardapio[]>(`${this.API_HOST}/cardapio`, { params });
   }
 
   cadastrar(cardapio: Cardapio): Observable<Cardapio> {
-    return this.http.post<Cardapio>(this.API, cardapio);
+    return this.http.post<Cardapio>(`${this.API_HOST}/cardapio`, cardapio);
   }
 }
--- src/app/pages/cardapio/lista-cardapio/lista-cardapio.component.ts
+++ src/app/pages/cardapio/lista-cardapio/lista-cardapio.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cardapio } from '../cardapio';
 import { CardapioService } from '../cardapio.service';
 
@@ -7,16 +7,14 @@ import { CardapioService } from '../cardapio.service';
   templateUrl: './lista-cardapio.component.html',
   styleUrls: ['./lista-cardapio.component.scss']
 })
-export class ListaCardapioComponent {
+export class ListaCardapioComponent implements OnInit {
 
   listaCardapio: Cardapio[] = [];
 
-  constructor(private cardapioService: CardapioService) {
-    this.onInit();
-  }
+  constructor(private cardapioService: CardapioService) { }
 
-  onInit(): void {
-    this.cardapioService.listar().subscribe((cardapioList) => {
+  ngOnInit(): void {
+    this.cardapioService.listar().subscribe(cardapioList => {
       this.listaCardapio = cardapioList
     })
   }
